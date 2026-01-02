7e08890f4f1d33bd5f217b94538029b58ab7211f
Jugal Rana <8733jugalrana@gmail.com>
2026-01-02 09:27:22 +0530

    .....

---
+++ JS TASK/1/1/26/task.js
@@ -0,0 +1,28 @@
+// 1. Create a Student class with a constructor that takes name and marks.
+// 2. Create an array of 3 student objects using the Student class.
+// 3. Use map() to display each student’s name and marks using template literals.
+// 4. Use find() to get the student who scored more than 60 marks.
+// // 5. Write one arrow function to add bonus marks (default value: 5)."
+class Student {
+  constructor(name, marks) {
+    this.name = name;
+    this.marks = marks;
+  }
+}
+
+// Array of 3 student objects
+const students = [
+  new Student("Jack", 40),
+  new Student("John", 70),
+  new Student("Jasus", 90)
+];
+
+// map() → display name and marks
+const studentDetails = students.map(
+  stud => `Name: ${stud.name}, Marks: ${stud.marks}`
+);
+console.log(studentDetails);
+
+// find() → student who scored more than 60
+const topper = students.find(stud => stud.marks > 60);
+console.log(topper);
