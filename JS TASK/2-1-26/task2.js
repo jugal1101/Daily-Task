c24a8bdc5991219b483acc0fe478f5fb485b08a8
Jugal Rana <8733jugalrana@gmail.com>
2026-01-02 16:15:03 +0530

    2/1/26

---
+++ JS TASK/2-1-26/task2.js
@@ -0,0 +1,34 @@
+// "1. Create an object called user with name and age.
+// 2. Inside the object, create a method that prints the user name using the this keyword (to show lexical scope understanding).
+// 3. Create two arrays (e.g., fruits and vegetables) and merge them into one array using the spread operator.
+// 4. Create a simple function that accepts a callback function and calls it after printing a message.
+// 5. Use an arrow function as the callback."
+
+
+let user = {
+    data : "user details",
+    name : "Jack",
+    age : "20",
+
+    userdata(){
+        document.writeln(this.name + "<br>")
+    }
+}
+user.userdata();
+
+
+let dryfruits = ["almond", "anjeer","cashew"];
+let pomefruit = ["Apple","kiwi","peach","pineapple"]
+
+let  winterspecial = [...dryfruits , ...pomefruit]
+console.log(winterspecial)
+document.writeln("[",winterspecial ,"]")
+
+
+function data(...a){
+    console.log(a)
+}
+
+data ("how are you",()=>{
+data()
+})
